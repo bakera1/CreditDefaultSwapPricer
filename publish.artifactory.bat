289a08ef5b5d53c9c628e96933486d26c2551f56
Alexander Baker <baker.alexander@gmail.com>
2018-11-03 15:33:30 +0000

    removing

--- publish.artifactory.bat
+++
@@ -1,6 +0,0 @@
-rem needed to play nicely with artifactory
-rem avoid No enum constant org.jfrog.repomd.pypi.model.PypiMetadata.MetadataVersion.v2_1
-rem pip install wheel==0.30.0
-rem pip install twine
-rem pip install setuptools==36.0.0
-twine upload --repository artifactory dist/* --verbose
